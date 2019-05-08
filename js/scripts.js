28075208a94a05a0054114b8f97d37014a5fb518
Dangol Dipendra <s1900618@edu.bc.fi>
2019-05-08 12:48:18 +0300

    added input and button

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,42 @@ $.getJSON( flickrAPI, {
         console.log(data);
     }
 );
+
+
+function handleResponse(resp){
+    console.log('The request has been completed');
+    console.log(resp);
+
+    if (resp.items.length === 0){
+        $('#imageWrapper').text('There is no image matching your search term');
+        return;
+    }
+
+
+    renderContent(resp);
+}
+
+function renderContent(resp){
+    $('#flickrLink').text(resp.link);
+    $('#flickrTitle').text(resp.title);
+
+    for (var i = 0; i < resp.items.length; i++){
+        $('#imageWrapper').append('<img src=" ' + url + '"></img>');
+    }
+}
+function doAjaxCall(){
+    $.getJSON(
+        flickrAPI,
+        paramsForFlickr,
+        handleResponse
+    );
+}
+
+$(document).ready(function(){
+    $('#search').on('click', function(){
+        var searchTerm = $('#searchTerm').val();
+        console.log(searchTerm);
+        doAjaxCall(searchTerm);
+    })
+})
+
